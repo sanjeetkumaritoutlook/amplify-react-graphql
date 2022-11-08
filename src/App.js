b1bbfe97a5728cb581cc0deaa6a0f043d34e6272
Kumar <sanjeet.a.kumar@capgemini.com>
2022-11-08 23:42:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          AWS Cloud Practitioner Sanjeet
         </a>
       </header>
     </div>
